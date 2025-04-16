c39707f226ca3e93dab60c464cedb44390a9ad28
Akhila <102479243+Akhila-Thada@users.noreply.github.com>
2025-04-16 08:34:21 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,31 +53,12 @@ const Navbar = () => {
         </a>
       </div>
       <div className='nav3'>
-      <div><a href="/" style={{
-            fontWeight: currentPath === '/' ? 'bold' : 'normal',
-            color: '#000',
-            textDecoration: 'none',
-          }}>>SHOP</a></div>
-      <div><a href="/" style={{
-            fontWeight: currentPath === '/' ? 'bold' : 'normal',
-            color: '#000',
-            textDecoration: 'none',
-          }}>>SKILLS</a></div>
-      <div><a href="/" style={{
-            fontWeight: currentPath === '/' ? 'bold' : 'normal',
-            color: '#000',
-            textDecoration: 'none',
-          }}>>STORIES</a></div>
-     <div> <a href="/" style={{
-            fontWeight: currentPath === '/' ? 'bold' : 'normal',
-            color: '#000',
-            textDecoration: 'none',
-          }}>>ABOUT</a></div>
-      <div><a href="/" style={{
-            fontWeight: currentPath === '/' ? 'bold' : 'normal',
-            color: '#000',
-            textDecoration: 'none',
-          }}>>CONTACT US</a></div>
+      <div><a href="/" >>HOME</a></div>
+      <div><a href="/" >>SHOP</a></div>
+      <div><a href="/">>SKILLS</a></div>
+      <div><a href="/" >>STORIES</a></div>
+     <div> <a href="/" }>>ABOUT</a></div>
+      <div><a href="/">>CONTACT US</a></div>
       </div>
 
 
